a188506b0b920213dd85c16c08abd4eafa3067e1
bulldozerat <bulldozerat@gmail.com>
2020-09-05 22:17:18 +0300

    mongoose, connect DB, Schema and Model

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection sucessfull'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const app = require('./app');
 
